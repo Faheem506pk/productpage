5b266f1ffcc6264a1f0e6c803391aff3b01891b4
Waqas Hussain <waqashussain91@gmail.com>
2024-02-27 09:13:59 +0500

    #update

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -2,6 +2,9 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   centeredSlides: false,
   slidesPerGroupSkip: 1,
+  loop: true,
+          speed: 700,
+          autoplay: true,
   grabCursor: true,
   keyboard: {
     enabled: true,
@@ -22,8 +25,12 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 var swiper = new Swiper(".productSwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
+  loop: true,
+          loopAddBlankSlides: true,
+          spaceBetween: 30,
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+          speed: 700,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
